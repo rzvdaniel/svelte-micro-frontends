5c685729bad927b3fbf6dd26175188eae3212965
Razvan Predescu <rzvdaniel@gmail.com>
2020-12-28 23:25:21 +0000

    Revert "Playing with a minimalistic version of the website" Revert last commit done by mistake This reverts commit a918fa7435e40d025956f2a0bb970fbe12c5e4c9.

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -2,52 +2,68 @@
 	export let segment;
 </script>
 
-<div class="opacity-nav">
-	<div class="menu-index" id="buttons" style="z-index:999999">
-		<i class="fa  fa-close" />
-	</div>
+<style>
+	nav {
+		border-bottom: 1px solid rgba(255, 62, 0, 0.1);
+		font-weight: 300;
+		padding: 0 1em;
+	}
 
-	<ul class="menu-fullscreen">
-		<li><a class="ajax-link" href="single.html">Home</a></li>
-		<li><a class="ajax-link" href="about-us.html">Pricing</a></li>
-		<li><a class="ajax-link" href="about-us.html">Download</a></li>
-		<li><a class="ajax-link" href="contact.html">Contact</a></li>
+	ul {
+		margin: 0;
+		padding: 0;
+	}
+
+	/* clearfix */
+	ul::after {
+		content: "";
+		display: block;
+		clear: both;
+	}
+
+	li {
+		display: block;
+		float: left;
+	}
+
+	[aria-current] {
+		position: relative;
+		display: inline-block;
+	}
+
+	[aria-current]::after {
+		position: absolute;
+		content: "";
+		width: calc(100% - 1em);
+		height: 2px;
+		background-color: rgb(255, 62, 0);
+		display: block;
+		bottom: -1px;
+	}
+
+	a {
+		text-decoration: none;
+		padding: 1em 0.5em;
+		display: block;
+	}
+</style>
+
+<nav>
+	<ul>
+		<li>
+			<a
+				aria-current={segment === undefined ? 'page' : undefined}
+				href=".">Home</a>
+		</li>
+		<li>
+			<a
+				aria-current={segment === 'app' ? 'page' : undefined}
+				href="apps/blog-post-add">Write Post</a>
+		</li>
+		<li>
+			<a
+				aria-current={segment === 'about' ? 'page' : undefined}
+				href="about">About</a>
+		</li>
 	</ul>
-</div>
-
-<!--Header-->
-<header id="fullscreen">
-	<div class="logo" id="full" style="display:none;">
-		<a class="ajax-link" href="index.html">RINKT STUDIO</a>
-	</div>
-
-	<div class="menu-index" style="display:none;" id="button">
-		<i class="fa fa-bars" />
-	</div>
-</header>
-
-<header class="boxed" id="header-white">
-	<div class="header-margin">
-		<div class="logo">
-			<a class="ajax-link" href="index.html">RINKT STUDIO</a>
-		</div>
-		<ul class="header-nav">
-			<li><a class="ajax-link" href="#">Pricing</a></li>
-			<li><a class="ajax-link" href="#">Download</a></li>
-			<li><a class="ajax-link" href="#">Contact</a></li>
-		</ul>
-
-		<ul class="social-icon">
-			<div class="social-index">
-				<li>
-					<a href="https://www.facebook.com/thomsooncom"><i
-							class="fa fa-facebook" /></a>
-				</li>
-				<li>
-					<a href="https://www.behance.net/TomaszMazurczak"><i
-							class="fa fa-behance" /></a>
-				</li>
-			</div>
-		</ul>
-	</div>
-</header>
+</nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,79 +1,9 @@
-<div class="clear" />
+<script>
+	import App from "./apps/app.svelte";
+</script>
 
-<!--Content-->
+<svelte:head>
+	<title>Blog posts</title>
+</svelte:head>
 
-<div class="content" id="ajax-content">
-	<div class="text-intro" id="site-type">
-		<h1>Software Automation</h1>
-		<h1 class="typewrite"><span>Projects</span></h1>
-		<!--Change on script.js-->
-		<p>
-			Rinkt Studio is a fully featured automation software to help
-			companies focus on the creative side of their business.
-		</p>
-	</div>
-
-	<!--Portfolio grid-->
-
-	<ul class="portfolio-grid" id="portfolio-sidebar">
-		<li class="grid-item">
-			<img src="img/portfolio/1.jpg" />
-			<a class="ajax-link" href="invoice">
-				<div class="grid-hover">
-					<h1>Single</h1>
-					<p>Branding</p>
-				</div>
-			</a>
-		</li>
-
-		<li class="grid-item">
-			<img src="img/portfolio/5.jpg" />
-			<a class="ajax-link" href="single-fullscreen.html">
-				<div class="grid-hover">
-					<h1>Full Screen</h1>
-					<p>Branding</p>
-				</div>
-			</a>
-		</li>
-
-		<li class="grid-item">
-			<img src="img/portfolio/2.jpg" />
-			<a class="ajax-link" href="#">
-				<div class="grid-hover">
-					<h1>Single</h1>
-					<p>Branding</p>
-				</div>
-			</a>
-		</li>
-
-		<li class="grid-item">
-			<img src="img/portfolio/3.jpg" />
-			<a class="ajax-link" href="#">
-				<div class="grid-hover">
-					<h1>Single</h1>
-					<p>Branding</p>
-				</div>
-			</a>
-		</li>
-
-		<li class="grid-item">
-			<img src="img/portfolio/4.jpg" />
-			<a class="ajax-link" href="#">
-				<div class="grid-hover">
-					<h1>Single</h1>
-					<p>Branding</p>
-				</div>
-			</a>
-		</li>
-
-		<li class="grid-item">
-			<img src="img/portfolio/6.jpg" />
-			<a class="ajax-link" href="#">
-				<div class="grid-hover">
-					<h1>Single</h1>
-					<p>Branding</p>
-				</div>
-			</a>
-		</li>
-	</ul>
-</div>
+<App app="blog-posts" page="" />
